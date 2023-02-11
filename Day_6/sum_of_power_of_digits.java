db48947455c043b4a7add809dca7978915cede1b
abishekvp <abishek.vp13@gmail.com>
2023-02-11 13:59:47 +0530

    commit9

---
+++ Day_6/sum_of_power_of_digits.java
@@ -0,0 +1,14 @@
+package Day_6;
+
+public class sum_of_power_of_digits {
+    public int addition(int input1, int input2){
+        double sum=0.0; 
+        String str=Integer.toString(input1);   
+        for(int i=0;i<str.length()-1;i++){
+            int a=Character.getNumericValue(str.charAt(i)); 
+            int b=Character.getNumericValue(str.charAt(i+1));   
+            sum=sum + Math.pow(a, b);
+        }
+        return (int)sum+1;
+    }
+}
